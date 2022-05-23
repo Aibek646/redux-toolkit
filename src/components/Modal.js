183210f1089ea8702911cdec7c9762b86027bf19
Aibek Ramazanov <aibekramazanov@Aibeks-MBP.lan>
2022-05-22 20:11:41 -0400

    last commit of the project

---
+++ src/components/Modal.js
@@ -0,0 +1,37 @@
+import { useDispatch } from "react-redux";
+import { closeModal } from "../features/modal/modalSlice";
+import { clearCart } from "../features/cart/cartSlice";
+
+const Modal = () => {
+    const dispatch = useDispatch();
+    return (
+        <aside className="modal-container">
+            <div className="modal">
+                <h4>remove all items form your shopping cart?</h4>
+                <div className="btn-container">
+                    <button
+                        type="button"
+                        className="btn confirm-btn"
+                        onClick={() => {
+                            dispatch(clearCart());
+                            dispatch(closeModal());
+                        }}
+                    >
+                        confirm
+                    </button>
+                    <button
+                        onClick={() => {
+                            dispatch(closeModal());
+                        }}
+                        type="button"
+                        className="btn clear-btn"
+                    >
+                        cancel
+                    </button>
+                </div>
+            </div>
+        </aside>
+    );
+};
+
+export default Modal;
